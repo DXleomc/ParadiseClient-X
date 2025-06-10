461885f415d358dd120031be07dd23d452062241
Zynox99 <167091582+DXleomc@users.noreply.github.com>
2025-06-10 11:29:18 +0530

    Update AuthMeVelocityPayloadPacket.java

--- src/main/java/io/github/spigotrce/paradiseclientfabric/packet/AuthMeVelocityPayloadPacket.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/packet/AuthMeVelocityPayloadPacket.java
@@ -2,27 +2,38 @@ package io.github.spigotrce.paradiseclientfabric.packet;
 
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.codec.PacketCodec;
 import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.util.Identifier;
 
-public record AuthMeVelocityPayloadPacket() implements CustomPayload {
-    public static final PacketCodec<PacketByteBuf, AuthMeVelocityPayloadPacket> CODEC = CustomPayload.codecOf(AuthMeVelocityPayloadPacket::write, AuthMeVelocityPayloadPacket::new);
+import java.util.UUID;
+
+public record AuthMeVelocityPayloadPacket(String spoofedUsername, UUID spoofedUUID, String spoofedIP) implements CustomPayload {
+    public static final PacketCodec<PacketByteBuf, AuthMeVelocityPayloadPacket> CODEC =
+            CustomPayload.codecOf(AuthMeVelocityPayloadPacket::write, AuthMeVelocityPayloadPacket::read);
+
     public static final Id<AuthMeVelocityPayloadPacket> ID = new Id<>(Identifier.of("authmevelocity", "main"));
 
-    private AuthMeVelocityPayloadPacket(PacketByteBuf buf) {
-        this();
+    // Reader
+    private static AuthMeVelocityPayloadPacket read(PacketByteBuf buf) {
+        String username = buf.readString(Short.MAX_VALUE);
+        UUID uuid = buf.readUuid();
+        String ip = buf.readString(Short.MAX_VALUE);
+        return new AuthMeVelocityPayloadPacket(username, uuid, ip);
     }
 
+    // Writer
     private void write(PacketByteBuf buf) {
         ByteArrayDataOutput out = ByteStreams.newDataOutput();
         out.writeUTF("LOGIN");
-        out.writeUTF(MinecraftClient.getInstance().getGameProfile().getName());
+        out.writeUTF(spoofedUsername);
+        out.writeUTF(spoofedUUID.toString());
+        out.writeUTF(spoofedIP);
         buf.writeBytes(out.toByteArray());
     }
 
+    @Override
     public CustomPayload.Id<AuthMeVelocityPayloadPacket> getId() {
         return ID;
     }
